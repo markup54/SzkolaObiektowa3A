9c14361caa32502d913ca0c6800488a85b6a92b7
markup54 <marzenakupka@tlen.pl>
2023-09-12 11:18:13 +0200

    wlasnoci w klasie osoba prywatne, metody dostepowe

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Osoba osoba = new Osoba();
-        osoba.imie = "Jan";
-        System.out.println("Imie naszej osoby"+osoba.imie);
-        osoba.wiek =-20;
-        System.out.println("Wiek"+osoba.wiek);
+        osoba.setImie("Jan");
+        System.out.println("Imie naszej osoby "+osoba.getImie());
+        osoba.setWiek(-20);
+        System.out.println("Wiek "+osoba.getWiek());
     }
 }
--- src/Osoba.java
+++ src/Osoba.java
@@ -1,5 +1,41 @@
 public class Osoba {
-    public String imie;
-    public String nazwisko;
-    public int wiek;
+    private String imie;
+    private String nazwisko;
+    private int wiek;
+
+    public String getImie() {
+        return imie;
+    }
+
+    public void setImie(String imie) {
+        if(imie.length()<3)
+            System.out.println("Imie musi mieć co najmniej 3 znaki");
+        else {
+            //TODO: sprawdzić czy tylko litery
+            //TODO: pierwsza litera duża pozostałe małe
+            this.imie = imie;
+        }
+    }
+
+    public String getNazwisko() {
+        return nazwisko;
+    }
+
+    public void setNazwisko(String nazwisko) {
+        this.nazwisko = nazwisko;
+    }
+
+    public int getWiek() {
+        return wiek;
+    }
+
+    public void setWiek(int wiek) {
+        if(wiek<0){
+            this.wiek =0;
+        }
+        else{
+            this.wiek = wiek;
+        }
+
+    }
 }
